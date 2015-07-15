70088d66f4bbf1970419d46d8ac6c84dbdbcfdf6
phonger <phonger@USERs-MacBook-Pro.local>
2015-07-15 14:31:14 -0600

    make change

--- hello_swift/ViewController.swift
+++ hello_swift/ViewController.swift
@@ -5,20 +5,11 @@
 //  Created by phonger on 2015-07-15.
 //  Copyright (c) 2015 phonger. All rights reserved.
 //
+// hi phong
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
